d2434d894fdc6239cbb83bd18f2b7832ce8723a6
ojixzzz <ojixzzz@gmail.com>
2015-05-18 11:09:47 +0700

    + waktu

--- service/server.py
+++ service/server.py
@@ -1,19 +1,22 @@
 import subprocess
+from datetime import datetime
 
 def cekservice():
     output = subprocess.check_output(['ps', '-A'])
+    waktu = str(datetime.now())
+
     if 'hhvm' not in output:
-        print("Peringatan service hhvm mati!")
+        print("[%s]Peringatan service hhvm mati!, mulai menghidupkan..." % waktu)
         print subprocess.check_output(['service', 'hhvm', 'start'])
 
     if 'apache2' not in output:
-        print("Peringatan service apache mati!")
+        print("[%s]]Peringatan service apache mati!, mulai menghidupkan..." % waktu)
         print subprocess.check_output(['service', 'apache2', 'start'])
 
     if 'mysql' not in output:
-        print("Peringatan service mysql mati!")
+        print("[%s]]Peringatan service mysql mati!, mulai menghidupkan..." % waktu)
         print subprocess.check_output(['service', 'mysql', 'start'])
 
     if 'ajenti-panel' not in output:
-        print("Peringatan service ajenti mati, mulai menghidupkan...")
+        print("[%s]]Peringatan service ajenti mati)
         print subprocess.check_output(['service', 'ajenti', 'start'])
