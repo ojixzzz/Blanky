79f1f7713771b3d84ed147cc1aee9e54ff416647
ojixzzz <ojixzzz@gmail.com>
2015-05-19 07:54:19 +0700

    kirim email

--- main.py
+++ main.py
@@ -1,9 +1,15 @@
 import time
 from service.server import cekservice
+from module.email import kirim_laporan
 
 # Entrypoint
 
-while True:
-    cekservice()
-    time.sleep(60)
+#while True:
+#    laporan = cekservice()
+#    if laporan != "":
+#        kirim_laporan('ojixzzz@gmail.com', laporan)
+#    time.sleep(60)
 
+laporan = cekservice()
+if laporan != "":
+    kirim_laporan('ojixzzz@gmail.com', laporan)
--- service/server.py
+++ service/server.py
@@ -4,23 +4,22 @@ from datetime import datetime
 def cekservice():
     output = subprocess.check_output(['ps', '-A'])
     waktu = str(datetime.now())
+    laporan = ""
 
     if 'hhvm' not in output:
-        print("[%s]Peringatan service hhvm mati!, mulai menghidupkan..." % waktu)
-        print subprocess.check_output(['service', 'hhvm', 'start'])
-        print "\n"
+        laporan = "%s [%s]Peringatan service hhvm mati!, mulai menghidupkan..." % (laporan, waktu)
+        laporan = "%s %s \n" % (laporan, subprocess.check_output(['service', 'hhvm', 'start']))
 
     if 'apache2' not in output:
-        print("[%s]Peringatan service apache mati!, mulai menghidupkan..." % waktu)
-        print subprocess.check_output(['service', 'apache2', 'start'])
-        print "\n"
+        laporan = "%s [%s]Peringatan service apache mati!, mulai menghidupkan..." % (laporan, waktu)
+        laporan = "%s %s \n" % (laporan, subprocess.check_output(['service', 'apache2', 'start']))
 
     if 'mysql' not in output:
-        print("[%s]Peringatan service mysql mati!, mulai menghidupkan..." % waktu)
-        print subprocess.check_output(['service', 'mysql', 'start'])
-        print "\n"
+        laporan = "%s [%s]Peringatan service mysql mati!, mulai menghidupkan..." % (laporan, waktu)
+        laporan = "%s %s \n" % (laporan, subprocess.check_output(['service', 'mysql', 'start']))"
 
     if 'ajenti-panel' not in output:
-        print("[%s]Peringatan service ajenti mati!, mulai menghidupkan..." % waktu)
-        print subprocess.check_output(['service', 'ajenti', 'start'])
-        print "\n"
+        laporan = "%s [%s]Peringatan service ajenti mati!, mulai menghidupkan..." % (laporan, waktu)
+        laporan = "%s %s \n" % (laporan, subprocess.check_output(['service', 'ajenti', 'start']))"
+    
+    return laporan
